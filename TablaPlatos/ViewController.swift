c733af7b144f02626912a89050d23ebbb7a614df
Camilo Alfonso <ca.alfonso967@uniandes.edu.co>
2017-09-20 16:50:11 -0500

    Initial Commit

---
+++ TablaPlatos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TablaPlatos
+//
+//  Created by Camilo Alfonso on 20/09/17.
+//  Copyright © 2017 Camilo Alfonso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
